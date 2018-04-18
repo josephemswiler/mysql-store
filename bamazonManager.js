ca578b221e2ac3b48385e25c088ac765e8079770
josephemswiler <josephemswiler@gmail.com>
2018-04-18 10:34:01 -0500

    add product

---
+++ bamazonManager.js
@@ -0,0 +1,109 @@
+let mysql = require('promise-mysql')
+let inquirer = require('inquirer')
+let Product = require('./product.js')
+let dbConnection = null
+
+mysql
+    .createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'bamazon'
+    })
+    .then(connectionMade => {
+        dbConnection = connectionMade
+        return runMgr()
+    })
+
+
+function runMgr() {
+    inquirer.prompt([{
+            type: 'list',
+            name: 'mgrChoice',
+            message: 'Select an option:',
+            choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product', 'Quit']
+        }])
+        .then(answer => {
+            console.log(answer.mgrChoice)
+            switch (answer.mgrChoice) {
+                case 'View Products for Sale':
+                    break
+                case 'View Low Inventory':
+                    break
+                case 'Add to Inventory':
+                    break
+                case 'Add New Product':
+                    addNew()
+                    break
+                case 'Quit':
+                    dbConnection.end()
+                    break
+            }
+        })
+}
+
+function addNew() {
+    inquirer.prompt([{
+                type: 'input',
+                name: 'name',
+                message: 'Enter name of new product:',
+                validate: function (input) {
+                    if (input === '') {
+                        console.log('\x1b[31m%s\x1b[0m', `Please enter a product name!`)
+                        return false
+                    }
+                    return true
+                }
+            },
+            {
+                type: 'input',
+                name: 'dept',
+                message: 'Enter department of new product:',
+                validate: function (input) {
+                    if (input === '') {
+                        console.log('\x1b[31m%s\x1b[0m', `Please enter a department!`)
+                        return false
+                    }
+                    return true
+                }
+            },
+            {
+                type: 'input',
+                name: 'price',
+                message: 'Enter price of new product:',
+                validate: function (input) {
+                    if (isNaN(input)) {
+                        console.log('\x1b[31m%s\x1b[0m', ` Please enter a number!`)
+                        return false
+                    } else if (input === '') {
+                        console.log('\x1b[31m%s\x1b[0m', `Please enter a price!`)
+                        return false
+                    }
+                    return true
+                }
+            },
+            {
+                type: 'input',
+                name: 'quantity',
+                message: 'Enter quantity of new product:',
+                validate: function (input) {
+                    if (isNaN(input)) {
+                        console.log('\x1b[31m%s\x1b[0m', ` Please enter a number!`)
+                        return false
+                    } else if (input === '') {
+                        console.log('\x1b[31m%s\x1b[0m', `Please enter a quantity!`)
+                        return false
+                    }
+                    return true
+                }
+            }
+        ])
+        .then(function (answers) {
+            let newProd = new Product(answers.name, answers.dept, answers.price, answers.quantity)
+            let query = 'INSERT INTO products SET ?'
+            dbConnection.query(query, newProd, function (err, res) {})
+            return answers.name
+        })
+        .then(prodAdded => console.log('\x1b[31m%s\x1b[0m', `The new product of ${prodAdded} has been added!`))
+        .then(next => runMgr())
+}
--- init.sql
+++ init.sql
@@ -8,10 +8,23 @@ CREATE TABLE products
     id INTEGER UNSIGNED AUTO_INCREMENT NOT NULL,
     product_name VARCHAR(50) NULL,
     department_name VARCHAR(50) NULL,
-    price INTEGER(10) NULL,
+    price FLOAT(10, 2) NULL,
     stock_quantity INTEGER(10) UNSIGNED NULL,
     PRIMARY KEY (id)
 );
 
 -- init db and create products table
 
+INSERT INTO products (
+    product_name, 
+    department_name, 
+    price, 
+    stock_quantity
+    )
+    VALUES 
+    (
+        'Circe',
+        'Books',
+        29.98,
+        800
+    );
